cc5dca9f0260a85a51f181a6be58e5b77ff50f07
bradleymaina <bradleymaina@proton.me>
2026-05-15 17:20:33 +0300

    add sqlite3 database

--- main.py
+++ main.py
@@ -1,21 +1,34 @@
-import os
+import sqlite3
 
-def get_serial_number():
-    pipe = os.popen('cat /sys/class/dmi/id/product_serial')
-    serialNumber = pipe.read().strip()
-    return serialNumber
-
-print(f"Serial Number: {get_serial_number()}")
+conn = sqlite3.connect("student.db")
+cursor = conn.cursor()
+cursor.execute("CREATE TABLE IF NOT EXISTS Students (name, regNo, laptopModel, serialNumber )")
 
 class Student:
-    def __init__(self,name, regNo, laptop_model ):
-        self.name= name
-        self.regNo = regNo
-        self.laptop_model = laptop_model
+    def __init__(self, db_connection, db_cursor):
+        self.conn = db_connection
+        self.cursor = db_cursor
 
-student1 = Student("Bradley", "CO28/404587/2024", "MSI Stealth gs66") 
+    def get_student_details(self):
+        name = input("Enter Name: ")
+        regNo = input("Enter Regitstration Number: ")
+        laptopModel = input("Enter Laptop Model: ")
+        serialNumber = input("Enter serial Number: ")
 
-print(student1.name, student1.regNo, student1.laptop_model)
+        self.cursor.execute("INSERT INTO Students VALUES (?, ?, ?, ?)", (name, regNo, laptopModel, serialNumber))
+        self.conn.commit()
+        print("Student details saved Successfully!")
 
 
+registry = Student(conn, cursor)
 
+while True:
+    registry.get_student_details()
+
+    choice = input("Want to keep adding records? (y/n): ")
+
+    if choice != 'y':
+     print("End of File")
+     break
+
+conn.close()
